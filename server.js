21e0c957802d648c3a0f877cf10b729b145f0c0b
Kitsuya0828 <kitsuyaazuma@gmail.com>
2022-03-25 00:31:48 +0900

    server challenge

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 // This is your test secret API key.
 const stripe = require('stripe')('sk_test_51KgpwYBEyNZnNN6yYGm63UuXMDbO7Piq7ngTlxGCtflcnoGlmCjAtP1y8VqKNH3e72hfZMuIMB02Mnvg3ICUVffQ00pyUmoWg3');
 const express = require('express');
+const path = require('path');
 const app = express();
-app.use(express.static('public'));
+const PORT = process.env.PORT || 4242
+app.use(express.static('public'))
+  .use(express.static(path.join(__dirname, 'public')))
+  .get('/', (req, res) => res.render('checkout'))
+
 
 const YOUR_DOMAIN = 'http://localhost:4242';
 
@@ -16,11 +21,11 @@ app.post('/create-checkout-session', async (req, res) => {
       },
     ],
     mode: 'payment',
-    success_url: `${YOUR_DOMAIN}/success.html`,
-    cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+    success_url: `./success.html`,
+    cancel_url: `./cancel.html`,
   });
 
   res.redirect(303, session.url);
 });
 
-app.listen(process.env.PORT || 4242, () => console.log('Running on port 4242'));
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
