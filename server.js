1f439f56d9e8ffb54d628da8e8d3b176c4d5df33
Kitsuya0828 <kitsuyaazuma@gmail.com>
2022-03-25 00:52:17 +0900

    server challenge

--- server.js
+++ server.js
@@ -2,12 +2,12 @@
 const stripe = require('stripe')('sk_test_51KgpwYBEyNZnNN6yYGm63UuXMDbO7Piq7ngTlxGCtflcnoGlmCjAtP1y8VqKNH3e72hfZMuIMB02Mnvg3ICUVffQ00pyUmoWg3');
 const express = require('express');
 const path = require('path');
-const app = express();
+
+const app = express(path.join(__dirname, 'public'));
 const PORT = process.env.PORT || 4242
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
+app.set(express.static(path))
+  .get("/", (req, res) => {res.sendFile(__dirname + "/index.html");});
 
 const YOUR_DOMAIN = 'http://localhost:4242';
 
@@ -21,8 +21,8 @@ app.post('/create-checkout-session', async (req, res) => {
       },
     ],
     mode: 'payment',
-    success_url: `./success.html`,
-    cancel_url: `./cancel.html`,
+    success_url: `https://azumaru-stripe.herokuapp.com/public/success.html`,
+    cancel_url: `https://azumaru-stripe.herokuapp.com/public/cancel.html`,
   });
 
   res.redirect(303, session.url);
