a27341c141eeafd25bdcbdfcf47c93cecfe19095
Kitsuya0828 <kitsuyaazuma@gmail.com>
2022-03-25 01:08:26 +0900

    server challenge

--- server.js
+++ server.js
@@ -4,13 +4,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const http = require("http");
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
-
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+app.use("/", (req, res) => {
+  res.sendFile("/index.html", {root:__dirname});
 });
 
 const YOUR_DOMAIN = 'http://localhost:4242';
